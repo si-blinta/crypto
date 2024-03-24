a8f077293b7209543d4f004fe809e259d2bf4e14
si-blinta <nader.ben.ammar.pro@gmail.com>
2024-03-24 21:33:18 +0100

    .

--- include/merkle_tree.h
+++ include/merkle_tree.h
@@ -35,7 +35,48 @@ merkle_tree* merkle_tree_build(uint8_t** data_blocks,size_t nb_blocks);
 
 
 
-
+/**
+ *                  Merkle tree building
+ * 
+ * Step 1 : Allocate 2d array
+ *      - depth = log2(number_of_blocks). take the upper bound: 3.2 -> 4   
+ *      - T = malloc( depth +1 ).
+ *      for (int i = 0; i < depth ; i++){
+ *          if(number_of_blocks % 2 == 1)
+ *              T[i] = malloc( number_of_blocks+1/i+1 );
+ *          else
+ *              T[i] = malloc( number_of_blocks / i);
+ *      }
+ *      - T[ depth ] = malloc(1);
+ * Step 2 : Hashing
+ *      //First hash round
+ *      for( int = 0; i < number_of_blocks ; i++){
+ *          T[0][i] = hash(data[i]);
+ *      }
+ *      if( size T[0] > number_of_blocks)
+ *          T[0][size-1] = T[0][size-2]
+ * 
+ *      //Hash the rest
+ *      for(int i = 0 ; i < depth; i++){
+ *          for(int j = 0 ; j < size of T[i] ){
+ *              T[i+1][j] = hash ( T[i][j*2] + T[i][ j*2 +1] );
+ *          
+ *          }
+ *          if( size T[i] % 2 == 1){
+ *              T[i]         = realloc(size T[i] + 1);
+ *              T[i][size-1] = T[i][size-2]
+ *          } 
+ *      }
+ *      T[ depth ] = hash (  T[depth-1][0] + T[depth-1][1]  ) LAST One 
+ *            
+ *      
+ * 
+ *
+ * 
+ * 
+ * 
+ * 
+*/
 
 
 
